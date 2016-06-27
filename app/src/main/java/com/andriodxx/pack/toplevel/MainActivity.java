a1724a7a72fa2a60ab31cf813013bea039803d61
topLevel <516288857@qq.com>
2016-06-27 11:48:52 +0800

    MainActivity加入了注释

--- app/src/main/java/com/andriodxx/pack/toplevel/MainActivity.java
+++ app/src/main/java/com/andriodxx/pack/toplevel/MainActivity.java
@@ -2,13 +2,18 @@ package com.andriodxx.pack.toplevel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //MainActivity
+        tv_show =(TextView)findViewById(R.id.tv_show);
+        tv_show.setText("这是字");
     }
 }
